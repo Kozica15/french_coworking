afd99dc2cbdc2c675f4aad534f424b00f571253b
claire fumey <claire.kozica@gmail.com>
2019-03-10 21:37:41 +0100

    validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+validates :name, presence :true
+validates :phone_number, presence :true
+validates :email, presence :true confirmation :true
+validates :biography, presence :true
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
